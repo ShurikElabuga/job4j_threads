af17dd1dd2dfa00406c909a56ecdd69ca393b51e
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-03-11 21:07:29 +0300

    3. Состояние нити. [#229175 #535541]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -13,7 +13,7 @@ public class ThreadState {
         System.out.println("second - " + second.getState());
         first.start();
         second.start();
-            while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
                 System.out.println("first - " + first.getState());
                 System.out.println("second - " + second.getState());
             }
