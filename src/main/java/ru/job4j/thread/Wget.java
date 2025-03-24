ef6c5d27043bf50be7c77eb0197f99e60392fe72
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-03-24 22:15:06 +0300

    8. Скачивание файла с ограничением. [#144271 #537185]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -9,15 +9,17 @@ public class Wget implements Runnable {
 
     private final String url;
     private final int speed;
+    private final String file;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String file) {
         this.url = url;
         this.speed = speed;
+        this.file = file;
     }
 
     @Override
     public void run() {
-        var file = new File("tmp.xml");
+        var file = new File(this.file);
         try (var input = new URL(url).openStream();
              var output = new FileOutputStream(file)) {
             var dataBuffer = new byte[1024];
@@ -27,27 +29,32 @@ public class Wget implements Runnable {
             while ((bytesRead = input.read(dataBuffer)) != -1) {
                 output.write(dataBuffer, 0, bytesRead);
                 totalBytes += bytesRead;
-                long downloadTime = System.currentTimeMillis() - startTime;
-                long expectedTime = totalBytes / speed;
-
-                if (expectedTime > downloadTime) {
-                    long pause = expectedTime - downloadTime;
-                    Thread.sleep(pause);
+                if (totalBytes >= speed) {
+                    long downloadTime = System.currentTimeMillis() - startTime;
+                    if (downloadTime < 1000) {
+                        try {
+                            Thread.sleep(1000 - downloadTime);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    startTime = System.currentTimeMillis();
+                    totalBytes = 0;
                 }
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length < 2) {
-            System.out.println("Проверьте ввод двух аргументов: <url> и <speed>");
-            return;
+        if (args.length < 3) {
+            throw new IllegalArgumentException();
         }
         String url = args[0];
+        String file = args[2];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, speed, file));
         wget.start();
         wget.join();
     }
